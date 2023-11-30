56d178a3b12f1dc1cddbac46bf30e87f04336f18
CaelusLel <113221213+CaelusLel@users.noreply.github.com>
2023-11-30 15:07:18 +0700

    Basic Understanding

---
+++ 1. Basic Learning/test.py
@@ -0,0 +1,11 @@
+# Contoh variabel dan tipe data
+nama = "John Doe"  # Variabel dengan tipe data string
+umur = 25         # Variabel dengan tipe data integer
+tinggi = 175.5    # Variabel dengan tipe data float
+is_married = False  # Variabel dengan tipe data boolean
+
+    # Menampilkan nilai variabel
+print("Nama:", nama)
+print("Umur:", umur)
+print("Tinggi:", tinggi)
+print("Status Pernikahan:", is_married)
