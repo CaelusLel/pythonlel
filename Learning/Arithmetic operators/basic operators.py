189aba0abefbb6fb7967382b5dfc4ceee4955ddf
CaelusLel <113221213+CaelusLel@users.noreply.github.com>
2023-11-30 16:35:55 +0700

    really basic

---
+++ Learning/Arithmetic operators/basic operators.py
@@ -0,0 +1,72 @@
+# Arithmetic operators
+x = 10  # Assigning 10 to variable x
+y = 5  # Assigning 5 to variable y
+
+# Performing arithmetic operations and storing the results in variables
+addition = x + y  # Addition
+subtraction = x - y  # Subtraction
+multiplication = x * y  # Multiplication
+division = x / y  # Division
+modulus = x % y  # Modulus (remainder of the division)
+exponentiation = x ** y  # Exponentiation (x to the power of y)
+floor_division = x // y  # Floor division (division that results in whole number adjusted to the left in the number line)
+
+# Printing the results of arithmetic operations
+print("Arithmetic operators:")
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
+print("Modulus:", modulus)
+print("Exponentiation:", exponentiation)
+print("Floor Division:", floor_division)
+
+# Boolean operators
+a = True  # Assigning True to variable a
+b = False  # Assigning False to variable b
+
+# Performing boolean operations and storing the results in variables
+and_operator = a and b  # AND operation
+or_operator = a or b  # OR operation
+not_operator = not a  # NOT operation
+
+# Printing the results of boolean operations
+print("\nBoolean operators:")
+print("AND Operator:", and_operator)
+print("OR Operator:", or_operator)
+print("NOT Operator:", not_operator)
+
+# Logic operators
+p = True  # Assigning True to variable p
+q = False  # Assigning False to variable q
+
+# Performing logic operations and storing the results in variables
+logical_and = p and q  # Logical AND operation
+logical_or = p or q  # Logical OR operation
+logical_not_p = not p  # Logical NOT operation on p
+logical_not_q = not q  # Logical NOT operation on q
+
+# Printing the results of logic operations
+print("\nLogic operators:")
+print("Logical AND:", logical_and)
+print("Logical OR:", logical_or)
+print("Logical NOT (p):", logical_not_p)
+print("Logical NOT (q):", logical_not_q)
+
+# Set operators
+set1 = {1, 2, 3, 4, 5}  # Defining set1
+set2 = {4, 5, 6, 7, 8}  # Defining set2
+
+# Performing set operations and storing the results in variables
+set_union = set1.union(set2)  # Union of set1 and set2
+set_intersection = set1.intersection(set2)  # Intersection of set1 and set2
+set_difference = set1.difference(set2)  # Difference of set1 and set2
+set_symmetric_difference = set1.symmetric_difference(set2)  # Symmetric difference of set1 and set2
+
+# Printing the results of set operations
+print("\nSet operators:")
+print("Set Union:", set_union)
+print("Set Intersection:", set_intersection)
+print("Set Difference:", set_difference)
+print("Set Symmetric Difference:", set_symmetric_difference)
+
