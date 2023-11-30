8bd6c690a247eb679e47c4c0f78b0f1283fbf6ff
CaelusLel <113221213+CaelusLel@users.noreply.github.com>
2023-11-30 16:43:43 +0700

    Implement

---
+++ Example/Arithmetic operators/basic operators.py
@@ -0,0 +1,33 @@
+
+# Real Life Implementation of Arithmetic Operators
+# Calculate the total cost of purchasing multiple items
+item1_price = 10.99
+item2_price = 5.99
+item3_price = 7.49
+
+total_cost = item1_price + item2_price + item3_price
+print("Total Cost:", total_cost)
+
+# Real Life Implementation of Boolean Operators
+# Check if a user is eligible for a discount
+age = 25
+is_student = True
+
+is_eligible_for_discount = age < 30 and is_student
+print("Eligible for Discount:", is_eligible_for_discount)
+
+# Real Life Implementation of Logic Operators
+# Check if a user has access to a restricted area
+has_access_key = True
+is_admin = False
+
+has_access_to_restricted_area = has_access_key or is_admin
+print("Has Access to Restricted Area:", has_access_to_restricted_area)
+
+# Real Life Implementation of Set Operators
+# Find common interests between two groups of people
+group1_interests = {"music", "sports", "movies"}
+group2_interests = {"sports", "books", "travel"}
+
+common_interests = group1_interests.intersection(group2_interests)
+print("Common Interests:", common_interests)
---
+++ Example/Arithmetic operators/basic variable.py
@@ -0,0 +1,41 @@
+# Variable declaration and assignment
+integer_var = 10  # Integer variable
+float_var = 3.14  # Float variable
+string_var = "Hello, World!"  # String variable
+boolean_var = True  # Boolean variable
+list_var = [1, 2, 3]  # List variable
+tuple_var = (4, 5, 6)  # Tuple variable
+set_var = {7, 8, 9}  # Set variable
+dict_var = {"name": "John", "age": 25}  # Dictionary variable
+
+# Print the values of the variables
+print("Integer variable:", integer_var)
+print("Float variable:", float_var)
+print("String variable:", string_var)
+print("Boolean variable:", boolean_var)
+print("List variable:", list_var)
+print("Tuple variable:", tuple_var)
+print("Set variable:", set_var)
+print("Dictionary variable:", dict_var)
+
+# Data type conversion
+integer_to_float = float(integer_var)  # Convert integer to float
+float_to_integer = int(float_var)  # Convert float to integer
+integer_to_string = str(integer_var)  # Convert integer to string
+boolean_to_string = str(boolean_var)  # Convert boolean to string
+
+# Print the converted values
+print("Integer to float:", integer_to_float)
+print("Float to integer:", float_to_integer)
+print("Integer to string:", integer_to_string)
+print("Boolean to string:", boolean_to_string)
+
+# Hexadecimal number
+hex_number = 0x1A  # Hexadecimal number (26 in decimal)
+
+# Complex number
+complex_number = 2 + 3j  # Complex number with real and imaginary parts
+
+# Print the values of the hexadecimal and complex numbers
+print("Hexadecimal number:", hex_number)
+print("Complex number:", complex_number)
